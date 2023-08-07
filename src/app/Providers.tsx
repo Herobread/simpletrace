ba5983ca51d928a67c5d7b05a6513d0ae5c8096b
Alex Nazarenko <alexeybread81@gmail.com>
2023-08-07 12:35:01 +0100

    add page loading progress

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,12 +1,22 @@
 'use client'
 
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 import { SessionProvider } from 'next-auth/react'
 import { SkeletonTheme } from 'react-loading-skeleton'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<SessionProvider>
-			<SkeletonTheme borderRadius={20}>{children}</SkeletonTheme>
+			<SkeletonTheme borderRadius={20}>
+				<ProgressBar
+					delay={200}
+					height="2px"
+					color="#7E52F7"
+					options={{ showSpinner: false }}
+					shallowRouting
+				/>
+				{children}
+			</SkeletonTheme>
 		</SessionProvider>
 	)
 }
