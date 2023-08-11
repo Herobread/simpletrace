41a460a49cca1eabb8a44acfda5f849ea64b67e2
Alex Nazarenko <alexeybread81@gmail.com>
2023-08-11 10:24:11 +0100

    remove test folder

--- src/app/(bugtracker)/projects/[id]/layout.tsx
+++ src/app/(bugtracker)/projects/[id]/layout.tsx
@@ -2,17 +2,12 @@ import Protected from '@/components/auth/Protected/Protected'
 
 interface ProjectsLayoutProps {
 	children: React.ReactNode
-	modal: React.ReactNode
 }
 
-export default function ProjectsLayout({
-	children,
-	modal,
-}: ProjectsLayoutProps) {
+export default function ProjectsLayout({ children }: ProjectsLayoutProps) {
 	return (
 		<>
 			<Protected redirectTo="/auth/login" user="only-authenticated" />
-			{modal}
 			{children}
 		</>
 	)
